3e4fb4e6439f9ac2743eab242c9719c0310fdeea
emnabelhaj <emna.belhaj@enis.tn>
2021-05-06 00:59:54 +0100

    Dexterity variance

--- Components/Count.js
+++ Components/Count.js
@@ -3,31 +3,13 @@ import { Text, View, StyleSheet, Animated } from 'react-native';
 import Constants from 'expo-constants';
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
 import array from './Dexterity';
+import count from './Dexterity';
 export default function Count() {
-  const [count, setCount] = React.useState(0)
+  //const [count, setCount] = React.useState(0)
  
   return (
     <View style={styles.container}>
-      <CountdownCircleTimer
-        size={100}
-        isPlaying
-        duration={30}
-        colors="#004777"
-        onComplete={() => {
-          console.log('ON_COMPLETE BEFORE RETURN')
-          return [false, 0]
-        }}
-      >
-        {({ remainingTime, animatedColor }) => ( 
-           
-            
-          <Animated.Text
-            style={{ ...styles.remainingTime, color: animatedColor }}>
-            {remainingTime}
-           
-          </Animated.Text>
-        )}
-      </CountdownCircleTimer>
+     
      
     </View>
    
@@ -46,10 +28,5 @@ const styles = StyleSheet.create({
     padding: 8,
   
 
-  },
-  remainingTime: {
-    fontSize: 20,
-
-  
-  },
+  }
 });
--- Components/Dexterity.js
+++ Components/Dexterity.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import Constants from 'expo-constants';
-import { LinearGradient } from 'expo-linear-gradient'
-import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
-import Count from '../Components/Count'
-let array = []
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Animated,
+} from "react-native";
+import Constants from "expo-constants";
+import { LinearGradient } from "expo-linear-gradient";
+import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
+import Count from "../Components/Count";
+
+let array = [];
+function variance(array) {
+  var len = 0;
+  var sum = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] == "") {
+    } else if (!isNum(array[i])) {
+      alert(array[i] + " is not number, Variance Calculation failed!");
+      return 0;
+    } else {
+      len = len + 1;
+      sum = sum + parseFloat(array[i]);
+    }
+  }
+  var v = 0;
+  if (len > 1) {
+    var mean = sum / len;
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] == "") {
+      } else {
+        v = v + (array[i] - mean) * (array[i] - mean);
+      }
+    }
+    return v / len;
+  } else {
+    return 0;
+  }
+}
+function update(countsPerSecond) {
+  array.push(countsPerSecond);
+  countsPerSecond = 0;
+}
 class Dexterity extends Component {
   constructor(props) {
     super(props);
@@ -13,83 +51,84 @@ class Dexterity extends Component {
 
   onPress = () => {
     this.setState({
-      count: this.state.count + 1
+      count: this.state.count + 1,
     });
-     
-     array.push((new Date()).getTime())
   
-     
   };
 
   render() {
     const { count } = this.state;
-    return ( 
+    return (
       <View style={styles.container}>
-      
-        <Count/>
-       
-        <View style={{flexDirection:'row', flexWrap:'wrap'}}>
-        <TouchableOpacity
-          style={styles.button1}
-          onPress={this.onPress}
+        <CountdownCircleTimer
+          size={100}
+          isPlaying={count >= 1}
+          duration={30}
+          colors="#004777"
+          onComplete={() => {
+            console.log(array);
+            return [false, 0];
+          }}
         >
-          
-    
-        </TouchableOpacity>
-        
-       </View>
-       <View style={styles.countContainer}>
-          <Text>Count: {count} {'\n'}
-           fréquence:{count/30}{'\n'}
-          </Text>
+          {({ remainingTime, animatedColor }) => (
+            
+            <Animated.Text
+              style={{ ...styles.remainingTime, color: animatedColor }}
+            > 
+              {remainingTime}
+              
+            </Animated.Text>
+          )}
+        </CountdownCircleTimer>
 
-       
+        <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+          <TouchableOpacity
+            style={styles.button1}
+            onPress={this.onPress}
+          ></TouchableOpacity>
         </View>
- 
+        <View style={styles.countContainer}>
+          <Text>
+            Count: {count} {"\n"}
+          </Text>
+        </View>
       </View>
-     
     );
-
-  
   }
- 
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop:20,
+    marginTop: 20,
     justifyContent: "center",
     paddingHorizontal: 10,
-
-    
   },
   button1: {
     alignItems: "center",
     backgroundColor: "#004777",
     padding: 40,
-    flex:1,
-    marginLeft:100,
-    marginRight:100,
-    borderRadius: 15
-
+    flex: 1,
+    marginLeft: 100,
+    marginRight: 100,
+    borderRadius: 15,
   },
   button2: {
     alignItems: "center",
     backgroundColor: "#DDDDDD",
     padding: 40,
-    flex:1,
-    marginRight:40,
-    marginLeft:10,
-    borderRadius: 15
-
+    flex: 1,
+    marginRight: 40,
+    marginLeft: 10,
+    borderRadius: 15,
   },
- 
+
   countContainer: {
     alignItems: "center",
-    padding: 10
-
- 
-  }
+    padding: 10,
+  },
+  remainingTime: {
+    fontSize: 20,
+  },
 });
 
 export default Dexterity;
